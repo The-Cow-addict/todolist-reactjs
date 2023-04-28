484d9fb7f88806e31a3231fd5e313c5cca23b8b7
The_Cow <ethanet26@gmail.com>
2023-04-29 01:22:41 +0800

    some self-documentation

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import TodoList from './TodoList'
 
 export default function App() {
   
+  //usestate initial value using a fat arrow function that gets 
+  //data from the local storage
   const [todos, settodos] = useState(() => {
     const localValue = localStorage.getItem("ITEM")
     if(localValue == null){
       return []
     }
     else{
+      //parse turns a json string to an object
       return JSON.parse(localValue)
     }
   })
 
+  //essentially setup as a listener to "todos"
+  //taking effect when "todos" have changed and 
+  //stringified into json and stored into local storage
   useEffect(() => {
     localStorage.setItem("ITEM", JSON.stringify(todos))
   }, [todos])
@@ -22,9 +28,10 @@ export default function App() {
   console.log(todos)
 
   function toggleTodo(id, completed) {
-    settodos(currentTodos => currentTodos.map(todo => {
-      if(todo.id === id) {
-        return {...todo, completed}
+    //this function sets the selected list to check marked or "completed"
+    settodos(currentTodos => currentTodos.map(todo => { //the mapping pulls out each individual element and 
+      if(todo.id === id) {                              //matches id
+        return {...todo, completed}                     //sets the state of the same id to completed
       }
       else {
         return todo
@@ -35,20 +42,23 @@ export default function App() {
   function addTodo(title) {
     settodos((currentTodos) =>{
         return[
+          //this creats a new set of current todos object array
+          //at the front of the new object array
         ...currentTodos, 
+        //this is where the new object comes into the array
         {
-           id : crypto.randomUUID(),
+           id : crypto.randomUUID(), //giving object with randomised id
            title,
-           completed : false
-          }
+           completed : false //setting the default state of the completing of
+          }                  //to false
         ]
       })
   }
 
   function deleteTodo(id) {
     settodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id != id)
-    })
+      return currentTodos.filter(todo => todo.id != id) //returns a new array without the id
+    })                                                  //that matches
   }
 
   return (
